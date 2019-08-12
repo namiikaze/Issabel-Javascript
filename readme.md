2f5efd21faa9c80fa78daef97264382af637aaad
Matheus <matheus.rangel10@hotmail.com>
2019-08-12 12:31:51 -0300

    first commit - send project

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-Hello World
+Issabel-Javascript
+
+Script para abrir o link do CRM ao cair ligações no ramal
+
+26/12/2017
---
+++ script.js
@@ -0,0 +1,30 @@
+var myVar = setInterval(verificarLigacao, 2000);
+
+console.log("|--------------------------------------|");
+console.log("|---------------CDC Cred---------------|")
+console.log("|-Script Integrar Issabel ao Panorama!-|");
+console.log("|----------Versão do script 1.4--------|");
+console.log("|--------------------------------------|");
+
+var verificarCPF = "000.000.000-00";
+var x = document.createElement("IFRAME");
+ 
+function verificarLigacao() {
+	  var valorDaDiv = $("#issabel-callcenter-estado-agente-texto").text();   
+	  console.log("Esperando ligação");
+	  if(valorDaDiv == "Connected to call"){			
+	  var conteudo = $(".ui-layout-center").text();    
+	  var cpf = conteudo.match(/[0-9]{3}[.][0-9]{3}[.][0-9]{3}[-][0-9]{2}/)		
+	  if(verificarCPF != String(cpf)){	
+	  	verificarCPF = cpf;
+		var valorDoRamal = $("#issabel-callcenter-titulo-consola").text();   
+		var ramal = parseInt(valorDoRamal.match(/[0-9]{3}/));
+		x.setAttribute("src", "http://cdccred.panoramaemprestimos.com.br/html.do?action=interacao&ramal="+String(ramal)+"&chave="+String(cpf)+"&telefone=&buscar=1");
+		x.setAttribute("Style", "opacity: 0.5;");
+		document.body.appendChild(x);	
+		
+		console.log("CPF:" +String(cpf) + " Ramal:" +String(ramal));
+		}
+	}
+}
+
